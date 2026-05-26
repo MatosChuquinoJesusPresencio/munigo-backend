262ace7faa5a8b76a1f9cf35de4e82b49690379c
JesusMatos <elcevichexd06@gmail.com>
2026-05-25 20:27:54 -0500

    update: arreglando importaciones del core

--- app/core/__init__.py
+++ app/core/__init__.py
@@ -16,15 +16,6 @@ from app.core.security import (
     decode_access_token,
     create_token_for_user,
 )
-from app.core.dependencies import (
-    get_current_user,
-    get_current_user_or_401,
-    require_role,
-    get_current_ciudadano,
-    get_current_funcionario,
-    get_current_inspector,
-    get_current_gerente,
-)
 
 __all__ = [
     "settings",
@@ -41,11 +32,4 @@ __all__ = [
     "create_access_token",
     "decode_access_token",
     "create_token_for_user",
-    "get_current_user",
-    "get_current_user_or_401",
-    "require_role",
-    "get_current_ciudadano",
-    "get_current_funcionario",
-    "get_current_inspector",
-    "get_current_gerente",
 ]
