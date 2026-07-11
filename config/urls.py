fd9af278b92b7571c83baf9672ffc46baef52c00
JesusMatos <elcevichexd06@gmail.com>
2026-07-11 18:32:30 -0500

    agregando endpoint de salud

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,18 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import JsonResponse
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
+def health_check(request):
+    return JsonResponse({"status": "ok"})
+
+
 urlpatterns = [
+    path('', health_check, name='health_check'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('users.urls')),
     path('api/organizations/', include('organizations.urls')),
